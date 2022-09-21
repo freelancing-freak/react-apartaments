6f9e6b2403e19991032fde8f332de60274ef6438
Robert Krzywina <robert.krzywina@gmail.com>
2022-09-21 23:28:07 +0200

    Add hero section

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,44 @@
+import {
+    ArrowForward,
+    ArrowRight,
+    HeroBg,
+    HeroBtnWrapper,
+    HeroContainer,
+    HeroContent,
+    HeroH1,
+    HeroImg,
+    HeroP
+} from "./HeroElements";
+import Image from '../../images/background.jpeg';
+import {useState} from "react";
+import {Button} from "../ButtonElement";
+
+const HeroSection = () => {
+
+    const [hover, setHover] = useState(false);
+
+    const onHover = () => {
+        setHover(!hover);
+    }
+
+    return (
+        <HeroContainer>
+            <HeroBg>
+                <HeroImg src={Image}/>
+            </HeroBg>
+            <HeroContent>
+                <HeroH1>Apartamenty na sprzedaż</HeroH1>
+                <HeroP>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                </HeroP>
+                <HeroBtnWrapper>
+                    <Button to='signup' onMouseEnter={onHover} onMouseLeave={onHover} primary='true' dark='true'>
+                        Zobacz ofertę {hover ? <ArrowForward/> : <ArrowRight/>}
+                    </Button>
+                </HeroBtnWrapper>
+            </HeroContent>
+        </HeroContainer>
+    );
+}
+
+export default HeroSection
