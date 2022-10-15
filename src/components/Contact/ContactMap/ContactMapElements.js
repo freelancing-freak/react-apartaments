024cdcedfe26cc028fb799933168b60939d36e62
Robert Krzywina <robert.krzywina@gmail.com>
2022-10-15 20:00:27 +0200

    Wrap contact map into Container

--- src/components/Contact/ContactMap/ContactMapElements.js
+++ src/components/Contact/ContactMap/ContactMapElements.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
+import {Container} from "react-bootstrap";
 
-export const ContactMapContainer = styled.div`
+export const ContactMapContainer = styled(Container)`
   margin-top: 30px;
 `;
