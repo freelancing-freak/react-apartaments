edec599a711d805592c6470a9c40ddad703e9088
Robert Krzywina <robert.krzywina@gmail.com>
2022-10-15 19:55:50 +0200

    Add example carousel in offer details

--- src/components/Offer/OfferList/OfferDetails/OfferDetailsElements.js
+++ src/components/Offer/OfferList/OfferDetails/OfferDetailsElements.js
@@ -2,6 +2,6 @@ import styled from "styled-components";
 import {Container} from "react-bootstrap";
 
 export const OfferDetailsContainer = styled(Container)`
-  margin-top: 80px;
+  margin-top: 100px;
   margin-bottom: 40px;
 `;
--- src/components/Offer/OfferList/OfferDetails/index.js
+++ src/components/Offer/OfferList/OfferDetails/index.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import {OfferDetailsContainer} from "./OfferDetailsElements";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import {Carousel} from "react-responsive-carousel";
+import './styles.css';
 
 const OfferDetails = ({apartment}) => {
+
+    window.scrollTo({top: 0, behavior: 'instant'});
+
     return (
         <OfferDetailsContainer>
-            <h1>Hello world! {apartment.id}</h1>
+            <div className='row'>
+                <div className='col-9'>
+                    <Carousel autoPlay={true} interval={2500} infiniteLoop={true} className='main-slide'>
+                        <div>
+                            <img src="https://hosting2279419.online.pro/images/apartment-1.jpeg"/>
+                        </div>
+                        <div>
+                            <img src="https://hosting2279419.online.pro/images/apartment-2.jpeg"/>
+                        </div>
+                        <div>
+                            <img src="https://hosting2279419.online.pro/images/apartment-3.jpeg"/>
+                        </div>
+                    </Carousel>
+                </div>
+                <div className='col-3'>
+                    <h1>Contact form!</h1>
+                </div>
+            </div>
         </OfferDetailsContainer>
     );
 }
