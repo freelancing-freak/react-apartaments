ef4c6c6c6d31d9611c39d88eae2dfa268935aad9
Robert Krzywina <robert.krzywina@gmail.com>
2022-10-16 17:29:05 +0200

    Add cookies alert

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {CookieConsent} from "react-cookie-consent";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <App/>
+        <CookieConsent buttonText="Akceptuję cookies"
+                       style={{background: "#2B373B"}}
+                       buttonStyle={{color: "#000", background: '#01bf71', fontSize: "13px"}}>
+            Informacja: Niniejszy serwis wykorzystuje do prawidłowego działania pliki cookies</CookieConsent>
     </React.StrictMode>
 );
