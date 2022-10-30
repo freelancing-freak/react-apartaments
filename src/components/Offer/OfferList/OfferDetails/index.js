2063ebe9d9aa61ffadcce8d712a376290141ebca
Robert Krzywina <robert.krzywina@gmail.com>
2022-10-30 19:34:15 +0100

    Changes on offer details page

--- src/components/Offer/OfferList/OfferDetails/index.js
+++ src/components/Offer/OfferList/OfferDetails/index.js
@@ -2,54 +2,65 @@ import React from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import {Carousel} from "react-responsive-carousel";
 import './styles.css';
+import ImageGallery from 'react-image-gallery';
+import {Button} from "../../../ButtonElement";
 
 const OfferDetails = ({apartment}) => {
 
+    const images = [
+        {
+            original: 'https://picsum.photos/id/1018/1000/600/',
+            thumbnail: 'https://picsum.photos/id/1018/250/150/',
+        },
+        {
+            original: 'https://picsum.photos/id/1015/1000/600/',
+            thumbnail: 'https://picsum.photos/id/1015/250/150/',
+        },
+        {
+            original: 'https://picsum.photos/id/1019/1000/600/',
+            thumbnail: 'https://picsum.photos/id/1019/250/150/',
+        },
+        {
+            original: 'https://picsum.photos/id/1018/1000/600/',
+            thumbnail: 'https://picsum.photos/id/1018/250/150/',
+        },
+        {
+            original: 'https://picsum.photos/id/1015/1000/600/',
+            thumbnail: 'https://picsum.photos/id/1015/250/150/',
+        },
+        {
+            original: 'https://picsum.photos/id/1019/1000/600/',
+            thumbnail: 'https://picsum.photos/id/1019/250/150/',
+        },
+        {
+            original: 'https://picsum.photos/id/1018/1000/600/',
+            thumbnail: 'https://picsum.photos/id/1018/250/150/',
+        },
+        {
+            original: 'https://picsum.photos/id/1015/1000/600/',
+            thumbnail: 'https://picsum.photos/id/1015/250/150/',
+        },
+        {
+            original: 'https://picsum.photos/id/1019/1000/600/',
+            thumbnail: 'https://picsum.photos/id/1019/250/150/',
+        },
+    ];
+
     window.scrollTo({top: 0, behavior: 'instant'});
     return (
-        <>
+        <div className='container'>
             <section id='prodetails' className='section-p1'>
                 <div className='single-pro-image'>
-                    <Carousel autoPlay={true} interval={2500} infiniteLoop={true} className='main-slide'>
-                        <div>
-                            <img src="https://hosting2279419.online.pro/images/apartment-1.jpeg"/>
-                        </div>
-                        <div>
-                            <img src="https://hosting2279419.online.pro/images/apartment-2.jpeg"/>
-                        </div>
-                        <div>
-                            <img src="https://hosting2279419.online.pro/images/apartment-3.jpeg"/>
-                        </div>
-                        <div>
-                            <img src="https://hosting2279419.online.pro/images/apartment-4.jpeg"/>
-                        </div>
-                        <div>
-                            <img src="https://hosting2279419.online.pro/images/apartment-5.jpeg"/>
-                        </div>
-                        <div>
-                            <img src="https://hosting2279419.online.pro/images/apartment-6.jpeg"/>
-                        </div>
-                        <div>
-                            <img src="https://hosting2279419.online.pro/images/apartment-7.jpeg"/>
-                        </div>
-                        <div>
-                            <img src="https://hosting2279419.online.pro/images/apartment-8.jpeg"/>
-                        </div>
-                    </Carousel>
-                </div>
-                <div className='single-pro-details'>
-                    <div className='single-pro-details-content'>
-                        <h6>Numer referencyjny: {apartment.id}</h6>
-                        <br/>
-                        <h4>{apartment.name}</h4>
-                        <h2>{apartment.price} €</h2>
-                        <h5>{apartment.measurement} M<sup>2</sup>,&nbsp;{apartment.numberOfRooms} pokoje</h5>
-                        <h5><i style={{fontSize: '20px'}} className="fa fa-map-marker"/> {apartment.location}</h5>
-                    </div>
+                    <h3>{apartment.name}</h3>
+                    <ImageGallery items={images} lazyload={true} showPlayButton={false} autoPlay={true} showNav={false} thumbnailPosition={'bottom'}/>
                 </div>
             </section>
             <section id='description'>
                 <h3>Szczegóły oferty</h3>
+                <h2>{apartment.price} €</h2>
+                <h5>{apartment.measurement} M<sup>2</sup>,&nbsp;{apartment.numberOfRooms} pokoje</h5>
+                <h5><i style={{fontSize: '20px'}} className="fa fa-map-marker"/> {apartment.location}</h5>
+                <br/>
                 <p style={{fontSize: '14px'}}>
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
                     industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
@@ -70,7 +81,7 @@ const OfferDetails = ({apartment}) => {
                     src={apartment.locationSrc}
                     width="100%" height="500px" frameBorder="0" style={{border: '0'}} allowFullScreen/>
             </section>
-        </>
+        </div>
     );
 }
 
