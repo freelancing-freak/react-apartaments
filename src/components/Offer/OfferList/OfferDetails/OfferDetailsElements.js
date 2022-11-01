a0b22ec48582a79cb799d00ca47aa351620b0ca8
Robert Krzywina <robert.krzywina@gmail.com>
2022-11-01 16:47:38 +0100

    Changes in offer details page

--- src/components/Offer/OfferList/OfferDetails/OfferDetailsElements.js
+++ src/components/Offer/OfferList/OfferDetails/OfferDetailsElements.js
@@ -5,3 +5,15 @@ export const OfferDetailsContainer = styled(Container)`
   margin-top: 100px;
   margin-bottom: 40px;
 `;
+
+export const H1 = styled.p`
+  --vh: 19.74px;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+  font-family: inherit;
+  letter-spacing: 0.5px;
+  color: rgb(0, 47, 52);
+  font-size: 20px;
+  line-height: 24px;
+  font-weight: normal;
+`;
--- src/components/Offer/OfferList/OfferDetails/index.js
+++ src/components/Offer/OfferList/OfferDetails/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import './styles.css';
 import ImageGallery from 'react-image-gallery';
-import {OfferDetailsContainer} from "./OfferDetailsElements";
+import {H1} from "./OfferDetailsElements";
 
 const OfferDetails = ({apartment}) => {
 
@@ -48,18 +48,35 @@ const OfferDetails = ({apartment}) => {
     window.scrollTo({top: 0, behavior: 'instant'});
     return (
         <>
-            <main>
+            <main className='container'>
                 <section className="content">
-                    <div className="image-gallery">
-                        <ImageGallery items={images} lazyload={true} showPlayButton={false} autoPlay={true} showNav={false} thumbnailPosition={'right'}/>
+                    <ImageGallery items={images} lazyload={true} showPlayButton={false} autoPlay={true} showNav={false}
+                                  thumbnailPosition={'right'}/>
+                    <p className='abc'>
+                        Dodane 28 października 2022
+                    </p>
+                    <p className='price'>
+                        {apartment.price} €
+                    </p>
+                    <H1>{apartment.name}</H1>
+                    <div className='location'>
+                        <i style={{fontSize: '20px'}} className="fa fa-map-marker"/>
+                        <b>&nbsp;{apartment.location}</b>
                     </div>
+                    <ul className="css-sfcl1s">
+                        <li className="css-ox1ptj"><p className="css-xl6fe0-Text eu5v0x0">
+                            <span>{apartment.measurement} M<sup>2</sup></span></p></li>
+                        <li className="css-ox1ptj"><p
+                            className="css-xl6fe0-Text eu5v0x0">{apartment.numberOfRooms} pokoje</p></li>
+                    </ul>
                 </section>
                 <aside className="form-contact">
                     <h2>Skontaktuj się</h2>
                     <input type="text" className="field" placeholder="Imię"/>
                     <input type="text" className="field" placeholder="E-mail"/>
                     <input type="text" className="field" placeholder="Telefon"/>
-                    <textarea placeholder="Wiadomość" value='Dzień dobry, zainteresowała mnie ta oferta.' className="field"/>
+                    <textarea placeholder="Wiadomość" value='Dzień dobry, zainteresowała mnie ta oferta.'
+                              className="field"/>
                     <button className="btn">Wyślij</button>
                 </aside>
             </main>
