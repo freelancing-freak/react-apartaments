48402cb0ff1c2c2a8d3129e9f787e63e46fe22ed
Robert Krzywina <robert.krzywina@gmail.com>
2022-11-01 17:22:10 +0100

    Changes in offer details page

--- src/components/Offer/OfferList/OfferDetails/index.js
+++ src/components/Offer/OfferList/OfferDetails/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import './styles.css';
 import ImageGallery from 'react-image-gallery';
@@ -45,7 +45,9 @@ const OfferDetails = ({apartment}) => {
         },
     ];
 
-    window.scrollTo({top: 0, behavior: 'instant'});
+    const [showMore, setShowMore] = useState(false);
+
+    let text = apartment.description;
     return (
         <>
             <main className='container'>
@@ -69,6 +71,29 @@ const OfferDetails = ({apartment}) => {
                         <li className="css-ox1ptj"><p
                             className="css-xl6fe0-Text eu5v0x0">{apartment.numberOfRooms} pokoje</p></li>
                     </ul>
+                    <br/>
+                    <H1>Opis</H1>
+                    <hr/>
+                    {
+                        text.length <= 250 ?
+                            <h6>
+                                {text}
+                            </h6> :
+                            <h6>
+                                {showMore ? text : `${text.substring(0, 400)} ...`}
+                                <br/>
+                                <br/>
+                                <button className='btnn' onClick={() => setShowMore(!showMore)}>
+                                    {showMore ? 'Skróć opis' : 'Zobacz więcej'}
+                                </button>
+                            </h6>
+                    }
+                    <br/>
+                    <H1>Lokalizacja</H1>
+                    <hr/>
+                    <iframe
+                        src={apartment.locationSrc}
+                        width="100%" height="500px" frameBorder="0" style={{border: '0'}} allowFullScreen/>
                 </section>
                 <aside className="form-contact">
                     <h2>Skontaktuj się</h2>
